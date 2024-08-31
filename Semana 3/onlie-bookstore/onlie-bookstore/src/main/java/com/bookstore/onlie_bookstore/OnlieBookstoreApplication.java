b916a3c51af050b4058e8876cac2d6a5cd341b98
Frida Cano Falcón <a01752953@tec.mx>
2024-08-31 17:49:15 -0600

    JPA project

---
+++ Semana 3/onlie-bookstore/onlie-bookstore/src/main/java/com/bookstore/onlie_bookstore/OnlieBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore.onlie_bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlieBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlieBookstoreApplication.class, args);
+	}
+
+}
