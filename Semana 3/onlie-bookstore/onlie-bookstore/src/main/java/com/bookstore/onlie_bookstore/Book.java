046b52fec55056623cfbb1e147cade8b7dc926b1
Frida Cano Falcón <a01752953@tec.mx>
2024-09-02 00:56:17 -0600

    JPA project act

---
+++ Semana 3/onlie-bookstore/onlie-bookstore/src/main/java/com/bookstore/onlie_bookstore/Book.java
@@ -0,0 +1,65 @@
+package com.bookstore.onlie_bookstore;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private Double price;
+
+    // Constructors, getters, and setters
+
+    public Book() {
+    }
+
+    public Book(String title, String author, Double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
